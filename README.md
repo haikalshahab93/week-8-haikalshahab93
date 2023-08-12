aefde5f3f5f94eeb4c5b0115b3274702efff8c7a
haikalshahab93 <haikal.pusri@gmail.com>
2023-08-12 08:37:33 +0700

    update data

---
+++ README.md
@@ -0,0 +1,2 @@
+# week-8-haikalshahab93
+week-8-haikalshahab93 created by GitHub Classroom
---
+++ dist/data.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.financial = void 0;
+let financial = [
+    { id: 1, type: 'Cash In', finance: 'Belanja', detail: 'Belanja Bulanan', cash: 4000000 },
+    { id: 2, type: 'Cash Out', finance: 'Gaji', detail: 'Bonus Keuntungan', cash: 500000 },
+    { id: 3, type: 'Cash Out', finance: 'Bayaran Tanggungan', detail: 'Bayar Cicilan Rumah', cash: 250000 },
+];
+exports.financial = financial;
